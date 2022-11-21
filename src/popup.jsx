4a470dd86c7b5a532fc067666857345981b5d73a
dannyray44 <dannyray44@hotmail.co.uk>
2022-11-21 20:09:54 +0100

    working for firefox development

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { render } from "react-dom";
+// import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 function Popup() {
 	return (
 		<div>
-			<h1>Hello world!</h1>
+			<h1>Hello world!!</h1>
 			<p>This is a simple popup!</p>
 		</div>
 	);
 }
 
-render(<Popup />, document.getElementById("react-target"));
+// render(<Popup />, document.getElementById("react-target"));
+
+const container = document.getElementById("react-target");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Popup />);
